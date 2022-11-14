843c9d5c997108991ce06d83295ab4f815ef2371
Koen Ponse <koen_ponse@hotmail.com>
2022-11-14 19:32:40 +0100

    allow consecutive runs with incrementing seeds

--- experiment.py
+++ experiment.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description='Experiment on AGAC')
 parser.add_argument('--env', type=str, default=default_env, help='Gym environment.')
 parser.add_argument('--timesteps', default=200000000, type=int, help='Number of timesteps to run.')
 parser.add_argument('--seed', default=default_seed, type=int, help='Random seed (default: 4949).')
+parser.add_argument('--num_runs', default=1, type=int, help='Number of consecutive runs. Will increment seed by 1 for each run (default: 1).')
 parser.add_argument('--policy', default="CnnPolicy", type=str, help='Use random adv.', choices=["CnnPolicy", "CnnLstmPolicy"])
 parser.add_argument('--n_frame_stack', default=4, type=int, help='Amount of stacked frames.')
 parser.add_argument('--rnd_adv', default=False, type=bool, help='Add Random adversary (default: False).')
@@ -20,14 +21,16 @@ parser.add_argument('--log_prefix', default="", type=str, help='Log prefix (defa
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    run_minigrid(
-        env_id=args.env, 
-        num_timesteps=args.timesteps, 
-        seed=args.seed, 
-        policy=args.policy,
-        n_frame_stack=args.n_frame_stack,
-        rnd_adv=args.rnd_adv,
-        add_noise_old=args.add_noise_old,
-        add_noise_normal=args.add_noise_normal,
-        log_prefix=args.log_prefix
-    )
+    seeds = [args.seed + i for i in range(args.num_runs)]
+    for seed in seeds:
+        run_minigrid(
+            env_id=args.env, 
+            num_timesteps=args.timesteps, 
+            seed=seed, 
+            policy=args.policy,
+            n_frame_stack=args.n_frame_stack,
+            rnd_adv=args.rnd_adv,
+            add_noise_old=args.add_noise_old,
+            add_noise_normal=args.add_noise_normal,
+            log_prefix=args.log_prefix
+        )
